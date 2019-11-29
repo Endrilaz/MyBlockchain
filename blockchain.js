1d7c41a4aaa88a1bb6dd077b9b245fc27561aae9
Endrilaz <zakaria.bouhaeb@epitech.eu>
2019-11-29 19:12:41 +0800

    jumped difficulty fix

--- blockchain.js
+++ blockchain.js
@@ -37,14 +37,16 @@ class Blockchain {
 
         for (var i = 1; i < chain.length; i++) {
             const { timeStamp, lastHash, hash, nonce, difficulty, data } = chain[i];
-
             const actualLastHash = chain[i-1].hash;
+            const lastDifficulty = chain[i-1].difficulty;
 
             if (lastHash !== actualLastHash) return false;
 
             const validateHash = cryptoHash(timeStamp, lastHash, data, nonce, difficulty );
 
             if (hash !== validateHash) return false;
+
+            if(Math.abs(lastDifficulty - difficulty) > 1) return false;
         }
 
         return true;
